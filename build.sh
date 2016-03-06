56a0bff21cfcb530dd8d539bc7375ff4868bcf55
Clive.liu <clive.liu@clivetech.com>
2016-03-07 00:03:47 +0800

    chore: 脚本参数检查

--- build.sh
+++ build.sh
@@ -4,6 +4,28 @@
 # All rights reserved.
 #
 
-make distclean
-make zynq_motion_config
+function error ()
+{
+    echo -e "\033[31mERROR: \033[0m"$1
+    exit 1
+}
+
+# Make sure that current directory is u-boot's path.
+CUR_PATH=`pwd`
+CUR_DIR=${CUR_PATH##*/}
+[ "${CUR_DIR}" != "u-boot-xlnx" ] && error "Current directory isn't u-boot's path."
+
+# Check environment variables
+[ -z ${ARCH} ] && error "Please perform \"$ source setup_env.sh\"."
+[ -z ${CROSS_COMPILE} ] && error "Please perform \"$ source setup_env.sh\"."
+
+# Clean files
+make ARCH=arm distclean
+# Build project
+make ARCH=arm zynq_motion_config
 make
+
+[ ! -e u-boot ] && error "Fail to build project."
+cp -af u-boot u-boot.elf
+
+exit 0
